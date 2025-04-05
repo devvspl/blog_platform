d474aad32fb5f6714cd7cc983fc507983c8ea440
devvspl <devendrakumar.vspl@gmail.com>
2025-04-05 21:53:33 +0530

    Upload files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\Tag;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
@@ -38,47 +35,5 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('password'),
             'role' => 'author',
         ]);
-
-        // Create 5 Categories
-        $categories = [];
-        for ($i = 1; $i <= 5; $i++) {
-            $name = 'Category ' . $i;
-            $categories[] = Category::create([
-                'name' => $name,
-                'slug' => strtolower(str_replace(' ', '-', $name)), // Generate slug
-            ]);
-        }
-
-
-        // Create 10 Posts and attach them to random categories
-        $posts = [];
-        foreach (range(1, 10) as $i) {
-            $title = 'Post Title ' . $i;
-            $posts[] = Post::create([
-                'title' => $title,
-                'slug' => strtolower(str_replace(' ', '-', $title)), // Generate slug
-                'content' => 'This is the content of post ' . $i,
-                'category_id' => $categories[array_rand($categories)]->id,
-                'user_id' => rand(1, 3),
-            ]);
-        }
-
-
-        // Create 15 Tags
-        $tags = [];
-        for ($i = 1; $i <= 15; $i++) {
-            $tagName = 'Tag ' . $i;
-            $tags[] = Tag::create([
-                'name' => $tagName,
-                'slug' => strtolower(str_replace(' ', '-', $tagName)), // Generate slug
-            ]);
-        }
-
-        // Attach tags to posts
-        foreach ($posts as $post) {
-            $post->tags()->attach(
-                collect($tags)->random(rand(1, 5))->pluck('id')->toArray()
-            );
-        }
     }
 }
